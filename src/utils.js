74ad7535ef612ccaf4a78d883f150d1de225458c
ikeoluwa fatimehin <ike_official@yahoo.com>
2023-09-08 17:28:07 +0000

    #commit to main

--- src/utils.js
+++ src/utils.js
@@ -4,8 +4,14 @@ export const range = (start, end, step = 1) => {
 
   // handle scenario where end is not specified range(5) - [0, 1, 2, 3, 4]
   // a conditional check for end
+  if (end === start) {
+    end = start;
+    start = 0;
+  }
+
 
   for (let i = start; i < end; i += step) {
+    output.push(i)
     // loop code
   }
 
